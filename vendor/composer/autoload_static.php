790286678fc5bf6efd58eaf1480f28da31825c5e
LaurynasAnd <l.andrulionis@gmail.com>
2020-10-15 09:35:29 +0300

    first commit

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit6b405389f6cf5c755d254844d9939af5
 {
+    public static $prefixLengthsPsr4 = array (
+        'L' => 
+        array (
+            'LaBank\\' => 7,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'LaBank\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/APP',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit6b405389f6cf5c755d254844d9939af5::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit6b405389f6cf5c755d254844d9939af5::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
